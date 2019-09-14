af068d75ad1aaa82afe6f6078b3638d81fe77886
Kelvin <kelvin@projx.it>
2019-09-15 00:48:21 +0100

    Initial commit, from fresh venv

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# squint
+# Squint - Blink Security Camera Scheduler
+
+## Background
+The Blink Security Cameras are pretty awesome, unfortunately, the scheduling turning monitoring on/off is lousy, its done at the Sync Module (Their Hub) level, so a schedule applies to every Camera associated with it. In a nutshell, its not possible to schedule individual cameras. 
+
+**What is the impact of this?** Say you have 6 cameras around your property, but only want the front door monitored during the day, well tough. you can't. Its all or nothing. Actually, there is one method, you have to buy an additional Sync Module just for that camera.
+
+This is not an oversight of Blink, literally hundreds of customers have made the same request over the past couple of years (Take a look at the blink community). As yet, Blink has not seen fit to comment on it. I won't attempt to decipher why. 
+
+After spending over £1000 on Blink Cameras, I didn't have much appetite for spending more, but for various reasons, we needed the ability to add specific schedules for 1 or 2 cameras, aswell as download the videos for long-term storage. 
+
+So, I put my weekend script-kiddie hat on, and this is the result. 
+
+## Introduction
+Squint is a script that allows the automation and control of individual Blink Cameras, with:
+ 
+ -[x] Multiple schedules for each camera for managing Motion Detection
+- [x] Automatically builds its own configuration file, based upon you Blink Cameras
+ -[ ] Scheduled downloading of new videos to your server/NAS/Laptop (WIP)
+ -[ ] Scheduled taking of new photo snaps
+
+# Warning
+AT MOST, SCHEDULE THIS SCRIPT TO RUN ONCE AN HOUR
+
+Blink do not publish their API details, they do no officially allow 3rd party access. But as long as they are not abused, they seem to unofficially tolerate it. 
+
+When doing the initial configuration make use of Squits "simulate" features, when live testing, leave 60 seconds between tests. Then when going live, minimise the number of times you run it, my recommendation is at most, once an hour. 
+
+You have been warned - Blink can and will disable accounts of customers who abuse this privilege. If this happens you will need to contact them to get it reactivated.
+
+## Installation
+When I get time, I'll add an autosetup.py, but the simplest installation is as follows:
+
+1. Create a python3.5+ virtualenv (My environment is python 3.7)
+2. Clone the repository, or download the zip file
+3. Run "pip install -r requirements.txt"
+
+## Usage 
+
+### Initial Configuration
+Squint automatically generate your initial configuration for you, it does retrieving your list of cameras from Blink, which it uses to build a boilerplate configuration file, this will include your credentials and a basic schedule for each Camera. 
+
+To do this, run the Squint with the following parameters:
+```
+squint.py config generate "<YOUR_BLINK_ID>" "<YOUR_BLINK_PASSWORD>"
+```
+This will generate the etc/squint.yml file, you can edit this with any text editor and tweak times and motion detection etc. 
+
+Notes:
+* Credentials are stored in plain text. Do not deploy this script onto a shared system. 
+* Running this command again, will force the existing file to be overwritten. Which is useful if you make a mistake, but be aware, you will lose any modifications you have made.
+
+### Testing / Simulating 
+In order to avoid the need to constantly connect to the Blink servers, Squint has a feature that allows you to save a copy of the data that Blink sends. You can then use this to test your configuration without bothering their servers, this can also be used for regenerating the configuration file.
+
+To pull the data from Blink and save it locally, run:
+```
+squint.py debug save 
+```
+This will generate the "blink_results.obj" which can now be used for testing. 
+To use the saved data, just run a command as you normally would. just pass one of the following switches
+ ```
+-t
+--testresponse
+```
+So for example, to regenerate the configuration file from the saved data, run the following
+```
+squint.py config generate "<YOUR_BLINK_ID>" "<YOUR_BLINK_PASSWORD>" --testresponse
+```
+To test your configuration against the saved data, run the following
+```
+squint.py push execute --testresponse
+```
+Please see below for more details on the "push execute"
+
+
+### Scheduling (Motion Detection)
+Once happy with the configuration, Squint can connect to the Blink servers, and attemmpt to apply the desired setting
+```
+squint.py push execute
+```
+In order to automate this on an ongoing basis, simple add this to your crontab or scheduler. Please do head the warning
+section above.
+
+### Scheduling (Clip Downloading)
+WIP
+### Scheduling (Photo Snaps)
+WIP
+
+## TODO
+-[ ] Implement clip downloading
+-[ ] Implement photo snaps
+-[ ] Add simulate against live (So changes are only indicated)
+
+
+
+ 
+## Thanks
+This scripts is 100% reliant upon work done by 
+
+* Blinkpy - https://github.com/fronzbot/blinkpy
+* Click - https://click.palletsprojects.com/
 
---
+++ squint.py
@@ -0,0 +1,320 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author:       Kelvin W
+# Date:         2019
+# Description:  A script for scheduling and managing individual Blink Security Cameras - A feature
+#               often requested, but yet to officially appear.
+#
+#               NOTE: this script uses an unofficial API, use this script wisely, if you hammer
+#                     the Blink servers, then they will likely disable your account. It is recommended
+#                     the scheduling features of this script, are set on a cron ran at most once an hour
+
+import yaml
+import json
+import pickle
+import sys
+import os
+import argparse
+import click
+import time
+import datetime
+
+from pprint import pprint
+from blinkpy import blinkpy
+
+
+"""
+GLOBAL CONSTANTS
+"""
+CONFIG_LOCAL_PATH = "/etc/squint.yml"
+
+
+"""
+SquintSchedulerConfig -
+
+All of Squints configuration is still in a single dictionary, this class is a wrapper for that, with 
+fuctions generating the config, loading and setting different attributes
+"""
+
+class SquintConfigManager():
+    format = ""
+    config = dict()
+    config_path = CONFIG_LOCAL_PATH
+
+    def __init__(self, data_format="yml"):
+        self.config["auth"] = dict()
+        self.config["cameras"] = dict()
+        self.format = data_format
+
+    def __serialise(self):
+        if self.format == "yml":
+            return yaml.dump(self.config,default_flow_style=False)
+        elif self.format == "json":
+            return json.dumps(self.config, indent=4)
+        else:
+            print("No config format specified")
+
+    def __unserialise(self, input):
+        if self.format == "yml":
+            return yaml.load(input, Loader=yaml.FullLoader)
+        elif self.format == "json":
+            return json.loads(input)
+        else:
+            print("No format specified")
+
+    def __get_os_path(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        return dir_path
+
+    def get_config_path(self):
+        return self.__get_os_path() + self.config_path
+
+    def get_username(self):
+        return self.config["auth"]["username"]
+
+    def get_password(self):
+        return self.config["auth"]["password"]
+
+    def save_to_file(self):
+        output = self.__serialise()
+        path = self.get_config_path()
+        with open(path, 'w') as filehandle:
+            filehandle.write(output)
+
+    def load_from_file(self):
+        path = self.get_config_path()
+        with open(path, 'r') as filehandle:
+            filecontent = filehandle.read()
+        
+        self.config = self.__unserialise(filecontent)
+
+    def add_credentials(self, username, password):
+        self.config["auth"]["username"] = username
+        self.config["auth"]["password"] = password
+
+        pprint(self.config)
+
+    def get_cameras(self):
+        return self.config["cameras"]
+
+    def add_camera(self, name, sync_module):
+        camera = dict()
+        camera["name"] = name
+        camera["sync_module"] = sync_module
+        camera["schedules"] = dict()
+        self.config["cameras"][name] = camera
+
+    def add_camera_schedule(self, camera_name, schedule_name, from_time, until_time, motion_detect):
+        schedule =  dict()
+        schedule["schedule_name"] = schedule_name
+        schedule["from_time"] = from_time
+        schedule["until_time"] = until_time
+        schedule["motion_detect"] = motion_detect
+        camera_config = self.config["cameras"][camera_name]
+        camera_config["schedules"][schedule_name] = schedule
+
+    def add_default_options(self):
+        """
+        Some default settings related to schedule execution, only used when building fresh configuration
+        """
+
+    def populate_from_blink(self, cameras, schedules):
+        """ 
+        Extracts camera and associated sync_module from dataset returned by Blink, these are used
+        to build out the basic config file. In addition, the schedule is used to add a boilerplate
+        schedule for each camera
+        
+        :param cameras: This is the Blink.cameras dict return by Blink 
+        :param schedules: A dict with boilderplate schedule for each camera
+        :return:
+        """
+        for name, camera in cameras.items():
+            sync_module = camera.attributes["sync_module"]
+            self.add_camera(name, sync_module)
+            for schedule in schedules:
+                self.add_camera_schedule(name, schedule["schedule_name"],
+                                             schedule["from_time"], schedule["until_time"], schedule["motion_detect"])
+
+
+class SquintConnectionManager:
+
+    use_local = False
+    blink = ""
+
+    def __init__(self, use_local = False):
+        self.use_local = use_local
+
+    def pickle_path(self):
+        return os.path.dirname(os.path.realpath(__file__)) + "/blink_results.obj"
+
+    def pickle_save(self):
+        with open(self.pickle_path(), 'wb') as filehandle:
+            pickle.dump(self.blink, filehandle)
+
+    def pickle_load(self):
+        with open(self.pickle_path(), 'rb') as filehandle:
+            self.blink = pickle.load(filehandle)
+
+    def connect(self, username, password):
+        if self.use_local==True:
+            click.echo("Loading locally Blink test object from " + self.pickle_path())
+            self.pickle_load()
+        else:
+            click.echo("Opening connection to Blink servers")
+            self.blink = blinkpy.Blink(username=username, password=password)
+            self.blink.start()
+
+    def set_motion_detect(self, camera_name, new_status):
+        camera = self.blink.cameras[camera_name]
+        current_status = camera.attributes["motion_enabled"]
+
+        if current_status != new_status:
+            print("{} is currently {}, changing this to {}".format(camera_name, current_status, new_status))
+            camera.set_motion_detect(new_status)
+            time.sleep(2)
+        else:
+            print("{} is already currently {}, leaving as-is".format(camera_name, current_status))
+
+
+class SquintScheduleManager:
+
+    def execute(self, connection, config):
+        cameras = config.get_cameras()
+        current_time = datetime.datetime.now().strftime("%H%M")
+
+        update_queue = dict()
+
+        # Extract schedules that we need to honour, these are added to updated_list, then actioned later
+        for camera_name, camera in cameras.items():
+            for schedule_name, schedule in camera["schedules"].items():
+                from_time = schedule["from_time"]
+                until_time = schedule["until_time"]
+                if  from_time <=  current_time <= until_time:
+                    click.echo('Added "{}" schedule {} to the queue'.format(camera_name, schedule_name))
+                    update_queue[camera_name] = schedule
+
+        if len(update_queue) == 0:
+            click.echo("No schedules found for this period")
+            return
+        else:
+            click.echo("Schedules Found: {}".format(len(update_queue)))
+            click.echo("connecting to Blink servers")
+            connection.connect(config.get_username(), config.get_password())
+
+            for camera_name, schedule in update_queue.items():
+                connection.set_motion_detect(camera_name, schedule["motion_detect"])
+
+
+@click.group()
+@click.version_option()
+def cli():
+    """Blink Manager
+    The cli tool for scheduling your Blink Cameras...
+    """
+
+
+"""
+ ARGS: Configuration
+"""
+@cli.group('config')
+def config():
+    """Manage the Blink Manager configuration file"""
+
+@config.command('generate')
+@click.argument('username')
+@click.argument('password')
+@click.option('--testresponse', "-t", is_flag=True, default=False, help='Test with locally saved response '
+                                                                 '(generated with "debug save"), rather '
+                                                                 'than contacting Blink servers')
+def config_generate(username, password, testresponse):
+    """Generates a basic config, including per-camera schedule. It does this by connecting to your Blink account
+    and extracting the necessary data.
+
+    Required Arguments:
+
+    USERNAME for your Blink Security account\n
+    PASSWORD for your Blink Security account
+    """
+
+    schedules = [{"schedule_name" : "00-NIGHT", "from_time":"0000", "until_time":'''0659''', "motion_detect" : True},
+                {"schedule_name" : "01-DAY","from_time":"0700", "until_time":"2259", "motion_detect" : False},
+                {"schedule_name" : "02-EVENING", "from_time":"2300", "until_time":"2359", "motion_detect" : True}]
+
+    click.echo("Opening connection to Blink servers")
+
+    config = SquintConfigManager("yml")
+    click.echo("Generating configuration")
+    config.add_credentials(username, password)
+
+    connection = SquintConnectionManager(testresponse)
+    connection.connect(config.get_username(), config.get_password())
+
+    config.populate_from_blink(connection.blink.cameras, schedules)
+    click.echo("Saving config to: " + config.get_config_path())
+    config.save_to_file()
+
+    click.echo("Finished saving!")
+    click.echo('The generated config is a boilerplate with a basic example schedule, '
+               'please update this to meets your needs. Once done you can use "execute simulate" command to test it')
+
+
+"""
+ ARGS: Push
+"""
+@cli.group('push')
+def push():
+    """Push configuration changes to Blink"""
+
+@push.command('execute')
+@click.option('--testresponse', "-t", is_flag=True, default=False, help='Test with locally saved response '
+                                                                 '(generated with "debug save"), rather '
+                                                                 'than contacting Blink servers')
+def  push_execute(testresponse):
+    """Execute the configuration push to Blink """
+    config = SquintConfigManager("yml")
+    config.load_from_file()
+
+    connection = SquintConnectionManager(testresponse)
+    scheduler = SquintScheduleManager()
+    scheduler.execute(connection, config)
+
+@push.command('simulate')
+@click.option('--testresponse', "-t", is_flag=True, default=False, help='Test with locally saved response '
+                                                                 '(generated with "debug save"), rather '
+                                                                 'than contacting Blink servers')
+def  push_simulate(testresponse):
+    """Simulate the configuration push to Blink"""
+    click.echo("Still deciding if this is needed.. for now, please use 'push execute -t' for testing")
+    # config = SquintConfigManager("yml")
+    # config.load_from_file()
+    #
+    # connection = SquintConnectionManager(testresponse)
+    # scheduler = SquintScheduleManager()
+    # scheduler.execute(connection, config)
+
+
+"""
+ ARGS: Debug
+"""
+@cli.group("debug")
+def debug():
+    """Avoid hammering Blink servers, save Blinks response locally testing"""
+
+@debug.command('save')
+def  debug_save():
+    """Retrieve data from Blink, save locally for testing"""
+    click.echo("Loading configuration")
+    config = SquintConfigManager("yml")
+    config.load_from_file()
+
+    click.echo("Opening connection to Blink servers")
+    connection = SquintConnectionManager()
+    connection.connect(config.get_username(), config.get_password())
+
+    click.echo("Saving Blink results to file")
+    connection.pickle_save()
+    click.echo("Blink results saved to " + connection.pickle_path())
+
+
+if __name__ == '__main__':
+    cli()
